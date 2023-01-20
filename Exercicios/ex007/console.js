0de4afeae8b078a6e110f4474f0d5510e5edce8e
Paulo Henrique Ferreira Lopes <paulohenrique1502@hotmail.com>
2023-01-19 23:35:20 -0300

    JS é complicado mas vdc

---
+++ Exercicios/ex007/console.js
@@ -0,0 +1,8 @@
+let num = [5,8,9]
+
+num[3]= 7// colocando na ultima linha
+num.push(3)// botando na ultima posição sem dizer
+num.length// cumprimento da variavel
+num.sort()//coloca em ordem crescente
+console.log(`nosso vetor é o ${num}`)
+console.log(num[0])

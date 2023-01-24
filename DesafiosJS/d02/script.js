75057b6ded0184aa0e559176bb5f73287696d3f9
Paulo Henrique Ferreira Lopes <paulohenrique1502@hotmail.com>
2023-01-23 23:50:00 -0300

    desafio 03

--- DesafiosJS/d02/script.js
+++ DesafiosJS/d02/script.js
@@ -1,6 +1,46 @@
 function verificar(){
     var data = new Date()
-    var ano = data.getFullYear
-    var nasc = document.querySelector(`div#iano`)
-    var  
+    var ano = data.getFullYear()
+    var nasc = document.querySelector(`input#ano`)
+    var res = document.querySelector(`div#res`)
+    var img = document.createElement(`img`)
+    if(nasc.value.length == 0 || nasc.value > ano){
+        window.alert(`Verifique Seus Dados`)
+    }else{
+        var fsex = document.getElementsByName(`sexo`)
+        var idade = ano - Number(nasc.value)
+        var genero = ``
+        var img = document.createElement(`img`)
+        img.setAttribute(`id`, `foto`)
+        if(fsex[0].checked){
+            genero = `Homem` 
+            if(idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute(`src`,`./image/criança.jpg`)
+                img.style.borderRadius = '50%'
+            }else if (idade < 21) {
+                //Jovem
+            }else if (idade < 50){
+                //adulto
+            }else{
+                // idoso
+            }
+        }else if (fsex[1].checked){
+            genero = `Mulher`
+            if(idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute(`src`,`./image/criançaf.jpg`)
+                img.style.borderRadius = '100%'
+            }else if (idade < 21) {
+                //Jovem
+            }else if (idade < 50){
+                //adulto
+            }else{
+                // idoso
+            }
+        }
+        res.style.textAlign = `Center`
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.` 
+        res.appendChild(img)
+    }
 }
---
+++ DesafiosJS/d03/script.js
@@ -0,0 +1,33 @@
+function contar(){
+    var inicio = document.querySelector(`input#inicioi`)
+    var fim = document.querySelector(`input#fimi`)
+    var passo = document.querySelector(`input#passoi`)
+    var res = document.querySelector(`div#res`)
+
+    
+
+    if(inicio.value.length == 0 || passo.value.length == 0 || fim.value.length == 0){
+        res.innerHTML = `impossivel contar!`
+    }else{
+        res.innerHTML = `Contando:`
+        var ini = Number(inicio.value)
+        var fi = Number(fim.value)
+        var pas = Number(passo.value)
+        if (pas <= 0){
+            window.alert("impossivel contar! considerando o passo 1")
+            pas = 1
+        }
+        if(ini < fi){
+            for(let c = ini; c <= fi; c += pas){
+                res.innerHTML += `${c}\u{1F449}`
+
+            }
+        }else{
+            for(let c = ini; c >= fi; c -= pas){
+                res.innerHTML += `${c}\u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
+

d08e5f9e81dc0aba59a5f484efa5de7884dfe519
Paulo Henrique Ferreira Lopes <paulohenrique1502@hotmail.com>
2023-01-25 23:38:42 -0300

    fim do desafio

--- DesafiosJS/d05/script.js
+++ DesafiosJS/d05/script.js
@@ -22,14 +22,43 @@ function inLista(n, l){
 
 function Adcionar(){
     if(isNumero(num.value) && !inLista(num.value, valores)){
-        window.alert(`okokokokokokokok`)
+        valores.push(Number(num.value))
+        var item = document.createElement(`option`)
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     }else{
-        window.alert(`Por favor, Digite um Número!`)
+        window.alert(`Valor Invalido ou Ja Encontrado na Lista!`)
     }
+    num.value = ''
+    num.focus()
         
     }
+   
 function Finalizar(){
-    if(num.value.length == 0 ){
+    if(valores.length == 0 ){
         window.alert(`Digite Um Valor Acima!`)
+    }else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores [pos]
+            
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `Ao todo temos ${tot} números`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>A soma dos Valores foi ${soma}</p>`
+        res.innerHTML += `<p>A Média dos Valores foi ${media}</p>`
     }       
 }
